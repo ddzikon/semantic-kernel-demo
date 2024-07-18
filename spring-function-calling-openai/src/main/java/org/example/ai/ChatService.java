d6a93049efe5235b08501d71c2c061564c6f94d5
Dariusz Dzikon <ddzikon@virtuslab.com>
2024-07-18 12:20:41 +0200

    connect to weather api

--- spring-function-calling-openai/src/main/java/org/example/ai/ChatService.java
+++ spring-function-calling-openai/src/main/java/org/example/ai/ChatService.java
@@ -1,24 +1,14 @@
 package org.example.ai;
 
-import lombok.NoArgsConstructor;
-import org.example.db.Person;
-import org.example.db.Repository;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.ai.model.function.FunctionCallbackWrapper;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.ai.openai.api.OpenAiApi;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Description;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 @Component
 public class ChatService {
@@ -26,7 +16,7 @@ public class ChatService {
     private static final String SPRING_AI_OPENAI_API_KEY;
 
     @Autowired
-    private Repository repository;
+    private OpenAiChatOptions openAiChatOptions;
 
     static {
         SPRING_AI_OPENAI_API_KEY = System.getenv("SPRING_AI_OPENAI_API_KEY");
@@ -39,52 +29,8 @@ public class ChatService {
     public String askGpt(String message) {
         OpenAiApi openAiApi = new OpenAiApi(SPRING_AI_OPENAI_API_KEY);
 
-        OpenAiChatOptions openAiChatOptions = OpenAiChatOptions.builder()
-                .withModel("gpt-3.5-turbo")
-//                .withFunction("myPersons")
-//                .withFunction("currentWeather")
-//                .withFunctionCallbacks(List.of(
-//                        new FunctionCallbackWrapper.Builder<>(new MyFunction())
-//                                .withName("myPersons")
-//                                .withDescription("Get my data.")
-////                                .withResponseConverter(persons -> persons.stream().map(Person::toString).collect(Collectors.joining(", ")))
-//                                .build(),
-//                        new FunctionCallbackWrapper.Builder<>(new MockWeatherService())
-//                                .withName("currentWeather")
-//                                .withDescription("Get the current weather in location")
-//                                .build()
-//                        )
-//                )
-                .build();
-
-
         ChatModel chatModel = new OpenAiChatModel(openAiApi, openAiChatOptions);
 
         return chatModel.call(new Prompt(message)).getResult().getOutput().getContent();
-    }
-
-    private static class MyFunction implements Function<MyFunction.Request, String> {
-
-        @Autowired
-        private Repository repository;
-
-        public record Request(){}
-
-        @Override
-        public String apply(MyFunction.Request o) {
-            return repository.findAll().stream()
-                    .map(Person::toString)
-                    .collect(Collectors.joining(", "));
-        }
-    }
-
-    @Configuration
-    static class Config {
-
-        @Bean
-        @Description("Get list of my persons")
-        public Function<MyFunction.Request, String> myPersons() {
-            return new MyFunction();
-        }
     }
 }
