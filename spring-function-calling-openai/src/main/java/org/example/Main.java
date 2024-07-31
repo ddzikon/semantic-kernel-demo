e026607f6754da52b146c3cb5e94f0b53fd2f7b1
Dariusz Dzikon <ddzikon@virtuslab.com>
2024-07-31 07:51:05 +0200

    add audio files handling

--- spring-function-calling-openai/src/main/java/org/example/Main.java
+++ spring-function-calling-openai/src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = { "org.example.ai", "org.example.controller", "org.example.db"})
+@SpringBootApplication(scanBasePackages = { "org.example.ai", "org.example.controller", "org.example.db", "org.example.audio"})
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
--- spring-function-calling-openai/src/main/java/org/example/controller/Controller.java
+++ spring-function-calling-openai/src/main/java/org/example/controller/Controller.java
@@ -1,12 +1,15 @@
 package org.example.controller;
 
+import org.example.ai.AudioChatService;
 import org.example.ai.ChatService;
+import org.example.audio.AudioFileResolver;
 import org.example.db.Person;
 import org.example.db.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,7 +21,13 @@ public class Controller {
     private Repository repository;
 
     @Autowired
+    private AudioFileResolver audioFileResolver;
+
+    @Autowired
     private ChatService chatService;
+
+    @Autowired
+    private AudioChatService audioChatService;
 
     @GetMapping("/hello")
     public String hello() {
@@ -33,5 +42,10 @@ public class Controller {
     @PostMapping("/ai/chat")
     public String askGpt(@RequestBody String message) {
         return chatService.askGpt(message);
+    }
+
+    @GetMapping("/ai/audio")
+    public String askWithAudioFile(@RequestParam String audioFilePath) {
+        return audioChatService.askGpt(audioFileResolver.getAudioFileHandler(audioFilePath));
     }
 }
