a36355e3a4b216d4b898b6a5a88f1e2a0ab1a6e8
Dariusz Dzikon <ddzikon@virtuslab.com>
2024-08-01 11:49:31 +0200

    add person manipulating functions

--- spring-function-calling-openai/src/main/java/org/example/db/Repository.java
+++ spring-function-calling-openai/src/main/java/org/example/db/Repository.java
@@ -2,6 +2,9 @@ package org.example.db;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface Repository extends JpaRepository<Person, Integer> {
 
+    List<Person> findByName(String name);
 }
