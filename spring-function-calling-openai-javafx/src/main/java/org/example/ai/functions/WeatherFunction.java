26f92c106156f263ff73e30c7b16e37c56a19ffc
ddzikon <77024881+ddzikon@users.noreply.github.com>
2024-09-09 09:35:29 +0200

    spring - javafx - clean up weather function (#25)

--- spring-function-calling-openai-javafx/src/main/java/org/example/ai/functions/WeatherFunction.java
+++ spring-function-calling-openai-javafx/src/main/java/org/example/ai/functions/WeatherFunction.java
@@ -11,24 +11,23 @@ import java.util.function.Function;
 @Slf4j
 @Component
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
-public class WeatherFunction implements Function<WeatherFunction.Request, WeatherFunction.Response> {
+public class WeatherFunction implements Function<WeatherFunction.Request, String> {
 
     public static final String FUNCTION_NAME = "currentWeather";
     public static final String FUNCTION_DESCRIPTION = "Get the current weather in location";
 
     public record Request(String location) {}
-    public record Response(String output) {}
 
     private final WeatherClient weatherClient;
 
     @Override
-    public Response apply(Request request) {
+    public String apply(Request request) {
         log.info("Requesting weather for request {}", request);
 
         final var weatherResponse = weatherClient.getCurrent(request.location);
 
         log.info("Current weather is:\n{}", weatherResponse);
 
-        return new Response(weatherResponse);
+        return weatherResponse;
     }
 }
